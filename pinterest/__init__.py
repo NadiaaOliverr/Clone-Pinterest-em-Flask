63e6443045a80db9dfeb5c7eeb991fccef218a27
Nádia Oliveira <nadiaaoliverr@gmail.com>
2023-10-27 13:08:10 -0300

    Criando escopo do banco de dados

--- pinterest/__init__.py
+++ pinterest/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+database = SQLAlchemy(app)
+
 
 from pinterest import routes
