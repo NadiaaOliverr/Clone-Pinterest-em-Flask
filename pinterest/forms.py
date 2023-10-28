5298ff454b478e949c79b18f4596bb5747380007
Nádia Oliveira <nadiaaoliverr@gmail.com>
2023-10-28 12:53:04 -0300

    Adicionando formulário de Login

---
+++ pinterest/forms.py
@@ -0,0 +1,23 @@
+from collections.abc import Mapping, Sequence
+from typing import Any
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from pinterest.models import Usuario
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer Login")
+
+class FormCriarConta(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    username = StringField("Username", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("Confirmação de senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("E-mail já cadastrado, faça login para continuar.")
