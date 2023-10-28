2d1f27d688d5cf66ed26f4a952484f87a5bc89de
Nádia Oliveira <nadiaaoliverr@gmail.com>
2023-10-28 12:36:57 -0300

    Preparando sistema de login

--- pinterest/__init__.py
+++ pinterest/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRECT_KEY"] = "0fd6cb167f8e8cd84b1189b0a495810e"
+
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 
 from pinterest import routes
--- pinterest/models.py
+++ pinterest/models.py
@@ -1,8 +1,13 @@
-from pinterest import database
+from pinterest import database, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
 
-class Usuario(database.Model):
+@login_manager.user_loader
+def load_usuario(id_usuario):
+    return Usuario.query.get(int(id_usuario))
+
+class Usuario(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key = True)
     username = database.Column(database.String, nullable = False, unique = True)
     email = database.Column(database.String, nullable = False, unique = True)
